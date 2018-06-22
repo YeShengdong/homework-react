890089a8a7f5b75e9a284ec0555a73b9a52831cd
Allen_Ye <Allen_Ye@epam.com>
2018-06-22 16:19:59 +0800

    up

--- src/server/app.js
+++ src/server/app.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const serverRenderer = require('../../dist/serverRenderer').default
 
 app.use(express.static('dist'))
 app.use('/static', express.static('static'))
 app.use('/test-report', express.static('jest/coverage/lcov-report'))
-// app.get('/', (req, res) => res.send('Hello World'))
-app.use((req, res) => {
-    const pa = path.resolve(__dirname, '../../dist/index.html')
-    console.log('pa', pa)
-    res.sendFile(pa)
-})
+//app.get('/', (req, res) => res.send('Hello World'))
+// app.use((req, res) => {
+//     const pa = path.resolve(__dirname, '../../dist/index.html')
+//     console.log('pa', pa)
+//     res.sendFile(pa)
+// })
+app.use(serverRenderer())
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
---
+++ webpack/webpack.config.server.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const webpack = require('webpack')
+// const HtmlWebpackPlugin = require('html-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const alias = require('../fileAlias')
+
+module.exports = (env, options) => {
+	const isProduction = env && env.prod ? true : false
+
+	const config = {
+		entry: './src/client/serverRenderer',
+		mode: isProduction ? 'production' : 'development',
+		devtool: isProduction ? 'none' : 'source-map',
+ 		externals: [nodeExternals()],
+		output: {
+			filename: 'serverRenderer.js',
+			libraryTarget: 'commonjs2',
+			path: path.resolve(__dirname, '../dist'),
+			publicPath: '/'
+		},
+
+		resolve: {
+			extensions: ['.js', '.jsx'],
+			alias: alias
+		},
+
+		module: {
+			rules: [
+				{
+		            test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+		            loader: 'url-loader',
+		            options: {
+						name: '[path][name].[ext]?[hash]'
+					}
+		        },
+				{
+					test: /\.jsx?$/,
+					loader: 'babel-loader',
+					exclude: /node_modules/
+				},
+		      	{
+					test: /\.scss$/,
+					use: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: [
+							{
+								loader: 'css-loader',
+								options: {
+		                            minimize: isProduction
+		                        }
+							},
+							{
+								loader: 'sass-loader',
+								options: {
+		                            minimize: isProduction
+		                        }
+							}
+						]
+					})
+				}
+			]
+		}
+	}
+
+	return config
+}
